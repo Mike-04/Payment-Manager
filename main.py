d23740d0796a7d050ca71a7c5f6449c19e6eb4fc
Mike-04 <hulubeacmihai@gmail.com>
2023-10-26 22:30:55 +0300

    wonderfull automated testing

--- main.py
+++ main.py
@@ -352,6 +352,31 @@ def total_by_key(payments,key):
                 total+=payments[nr][key]
         print("The total for the:",key,"is",total)
 
+def auto_testing():
+        keys=['gas','water','heat','sewage','misc']
+        sb_changes=[]
+        sb_payments={}
+        for tests in range(1,100):
+                for it in range(1,100):
+                        nr=random.randint(0,100)
+                        gas=float(random.randint(0,100))
+                        water=float(random.randint(0,100))
+                        heat=float(random.randint(0,100))
+                        sewage=float(random.randint(0,100))
+                        misc=float(random.randint(0,100))
+                        date=datetime.date.today()
+                        test={'gas': gas,'water':water,'heat':heat,'sewage':sewage,'misc':misc,'date':date}
+                        ADD(sb_payments,nr,test,sb_changes)
+                sb_payments_c=sb_payments
+                mass_MOD(sb_payments,min(sb_payments),max(sb_payments), random.choice(keys), float(random.randint(0,100)), sb_changes)
+                UNDO(sb_payments,sb_changes)
+                assert(sb_payments==sb_payments_c)
+                mass_DEL(sb_payments,min(sb_payments),max(sb_payments),sb_changes)
+                assert(sb_payments=={})
+                UNDO(sb_payments,sb_changes)
+                assert(sb_payments==sb_payments_c)
+
+
 def run():
         changes=[]
         payments={}
@@ -497,5 +522,5 @@ def run():
                                                         print("R U rlly a DEV?")
                         case _:
                                 print("Invalid input!")
-
+auto_testing()
 run()
